7af3b2cfc003a8dc9758cff1a963130df2cf195c
EmelinaTanya <62333591+EmelinaTanya@users.noreply.github.com>
2020-04-05 16:43:35 +0400

    Update Main.java
    fixed cod

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 public class Main {
     public static void main(String[] args) {
         float price = 3350;// стоимость авиабилета
-        float bonus_miles = price / 20;// порядок начисления бонусов, 1 миля за каждые 20 рублей 
-        System.out.println(bonus_miles);
+        float bonusMiles = price / 20;// порядок начисления бонусов, 1 миля за каждые 20 рублей 
+        System.out.println(bonusMiles);
     }
 }
